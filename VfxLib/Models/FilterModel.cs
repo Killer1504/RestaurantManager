45260dfa636095c892cf10c732ef5d83de65366d
Killer1504 <thanhhungbkhn@gmail.com>
2023-01-10 00:47:21 +0700

    implement filter, pagination model

--- VfxLib/Models/FilterModel.cs
+++ VfxLib/Models/FilterModel.cs
@@ -8,6 +8,54 @@ namespace VfxLib.Models
 {
     public class FilterModel : BasePropertyChangedNotification
     {
+        public List<string> ListKeyFilter
+        {
+            get => GetValue(() => ListKeyFilter);
+            set => SetValue(() => ListKeyFilter, value);
+        }
+
+        public string SeletedKeyFilter
+        {
+            get => GetValue(() => SeletedKeyFilter);
+            set => SetValue(() => SeletedKeyFilter, value);
+        }
+
+        public string KeySearch
+        {
+            get => GetValue(() => KeySearch);
+            set => SetValue(() => KeySearch, value);
+        }
+
+        public int CurrentIndexFilter
+        {
+            get => GetValue(() => CurrentIndexFilter);
+            set
+            {
+                SetValue(() => CurrentIndexFilter, value);
+                IsEnableNextFilter = CurrentIndexFilter > 0 && CurrentIndexFilter < LengthSearchedFilter;
+            }
+        }
+
+        public int LengthSearchedFilter
+        {
+            get => GetValue(() => LengthSearchedFilter);
+            set
+            {
+                SetValue(() => LengthSearchedFilter, value);
+                IsEnableNextFilter = CurrentIndexFilter > 0 && CurrentIndexFilter < LengthSearchedFilter;
+            }
+        }
+
+        public bool IsEnableNextFilter
+        {
+            get => GetValue(() => IsEnableNextFilter);
+            set => SetValue(() => IsEnableNextFilter, value);
+        }
+        public List<object> ListModelsFilter
+        {
+            get => GetValue(() => ListModelsFilter);
+            set => SetValue(() => ListModelsFilter, value);
+        }
 
     }
 }
--- VfxLib/Models/PaginationModel.cs
+++ VfxLib/Models/PaginationModel.cs
@@ -8,6 +8,73 @@ namespace VfxLib.Models
 {
     public class PaginationModel : BasePropertyChangedNotification
     {
+        public int CurrentPage
+        {
+            get => GetValue(() => CurrentPage);
+            set
+            {
+                SetValue(() => CurrentPage, value);
+                IsEnableNextPage = CurrentPage < TotalPage;
+                IsEnablePrePage = CurrentPage > 1;
+            }
+        }
 
+        public int PageSize
+        {
+            get => GetValue(() => PageSize);
+            set => SetValue(() => PageSize, value);
+        }
+
+        public int TotalPage
+        {
+            get => GetValue(() => TotalPage);
+            set
+            {
+                SetValue(() => TotalPage, value);
+                IsEnableNextPage = CurrentPage < TotalPage;
+                IsEnablePrePage = CurrentPage > 1;
+            }
+        }
+
+        public int TotalRecord
+        {
+            get => GetValue(() => TotalRecord);
+            set
+            {
+                SetValue(() => TotalRecord, value);
+                TotalPage = (int)Math.Ceiling(TotalRecord * 1.0 / PageSize);
+            }
+        }
+
+        public bool IsEnableNextPage
+        {
+            get => GetValue(() => IsEnableNextPage);
+            set => SetValue(() => IsEnableNextPage, value);
+        }
+
+        public bool IsEnablePrePage
+        {
+            get => GetValue(() => IsEnablePrePage);
+            set => SetValue(() => IsEnablePrePage, value);
+        }
+
+        public void Reset()
+        {
+            IsEnableNextPage = false;
+            IsEnablePrePage = false;
+            TotalRecord = 0;
+            TotalPage = 0;
+            PageSize = 500;
+            CurrentPage = 0;
+        }
+        public PaginationModel()
+        {
+            IsEnableNextPage = false;
+            IsEnablePrePage = false;
+            TotalRecord = 0;
+            TotalPage = 0;
+            PageSize = 500;
+            CurrentPage = 0;
+        }
     }
 }
